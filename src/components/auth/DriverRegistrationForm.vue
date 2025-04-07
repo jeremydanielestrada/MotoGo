6e6f45a7a25b33f970ede104b8a2ba4a1b34c244
Jaylameh-Gulay <jaylamehgulay318@gmail.com>
2025-04-07 21:43:25 +0800

    fixed: adding image, views and forms

---
+++ src/components/auth/DriverRegistrationForm.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-form fast-fail @submit.prevent>
+    <v-text-field label="First Name" type="text" variant="outlined"></v-text-field>
+
+    <v-text-field label="Last Name" type="password" variant="outlined"></v-text-field>
+
+    <v-text-field label="Phone Number" type="phone" variant="outlined"></v-text-field>
+
+    <v-text-field label="Password" type="password" variant="outlined"></v-text-field>
+
+    <v-text-field label="Confirm Password" type="password" variant="outlined"></v-text-field>
+
+    <v-file-input
+      :show-size="1000"
+      color="deep-purple-accent-4"
+      label="Upload License"
+      placeholder="Select your files"
+      prepend-icon="mdi-upload-outline"
+      counter
+      multiple
+      variant="plain"
+    >
+    </v-file-input>
+
+    <div class="d-flex justify-center">
+      <v-hover v-slot="{ isHovering, props }">
+        <v-btn
+          v-bind="props"
+          :color="isHovering ? 'purple-darken-1' : undefined"
+          class="mt-2 submit-button"
+          type="submit"
+          ripple
+          >Submit</v-btn
+        >
+      </v-hover>
+    </div>
+  </v-form>
+</template>
+<style scoped>
+.submit-button {
+  background-color: #e1bee7;
+}
+</style>
---
+++ src/components/auth/LoginForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-form fast-fail @submit.prevent>
+    <v-text-field label="User name" type="text" variant="outlined"></v-text-field>
+
+    <v-text-field label="Password" type="password" variant="outlined"></v-text-field>
+    <div class="d-flex justify-center">
+      <v-hover v-slot="{ isHovering, props }">
+        <v-btn
+          v-bind="props"
+          :color="isHovering ? 'purple-darken-1' : undefined"
+          class="mt-2 submit-button"
+          type="submit"
+          ripple
+          >Submit</v-btn
+        >
+      </v-hover>
+    </div>
+  </v-form>
+  <v-divider class="mt-5"> </v-divider>
+  <h5 class="mt-2">
+    Don't have an account?<RouterLink to="/register"> Register Here </RouterLink>
+  </h5>
+</template>
+<style scoped>
+.submit-button {
+  background-color: #e1bee7;
+}
+</style>
