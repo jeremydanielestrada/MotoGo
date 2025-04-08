d169295d523b7b8b581667c3e3d1bc436cb8446a
Jeremy Daniel Estrada <jeremydanielestrada2001@gmail.com>
2025-04-08 18:28:09 +0800

    Update configuration for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/MotoGo/' : '/',
+  base: process.env.VERCEL ? '/' : '/MotoGo/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
