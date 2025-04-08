7281d5d6e2f91805d19e85cd09e4267e1a2d7765
Jeremy Daniel Estrada <jeremydanielestrada2001@gmail.com>
2025-04-08 08:20:26 +0800

    feat:fetching main

--- src/components/auth/RegisterForm.vue
+++ src/components/auth/RegisterForm.vue
@@ -1,65 +1,4 @@
 <script setup>
-<<<<<<< HEAD
-const rules = {
-  required: (value) => !!value || 'Field is required',
-}
-</script>
-<template>
-  <v-form fast-fail @submit.prevent>
-    <v-text-field
-      label="First Name"
-      :rules="[rules.required]"
-      type="text"
-      variant="outlined"
-    ></v-text-field>
-    <v-text-field
-      label="Last Name"
-      :rules="[rules.required]"
-      type="text"
-      variant="outlined"
-    ></v-text-field>
-
-    <v-text-field
-      label="Phone Number"
-      :rules="[rules.required]"
-      type="phone"
-      variant="outlined"
-    ></v-text-field>
-
-    <v-text-field
-      label="Password"
-      :rules="[rules.required]"
-      type="password"
-      variant="outlined"
-    ></v-text-field>
-
-    <v-text-field
-      label="Confirm Password"
-      :rules="[rules.required]"
-      type="password"
-      variant="outlined"
-    ></v-text-field>
-
-    <div class="d-flex justify-center">
-      <v-hover v-slot="{ isHovering, props }">
-        <v-btn
-          v-bind="props"
-          :color="isHovering ? 'purple-darken-1' : undefined"
-          class="mt-2 submit-button"
-          type="submit"
-          ripple
-          >Submit</v-btn
-        >
-      </v-hover>
-    </div>
-  </v-form>
-</template>
-<style scoped>
-.submit-button {
-  background-color: #e1bee7;
-}
-</style>
-=======
 import { ref } from 'vue'
 import {
   confirmedValidator,
@@ -220,4 +159,3 @@ const onSubmit = () => {
     </v-row>
   </v-form>
 </template>
->>>>>>> origin/development
