519897657c164f3173036dde669afe83026777f7
Jaylameh-Gulay <jaylamehgulay318@gmail.com>
2025-04-18 14:31:27 +0800

    feat: adding path for message

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/views/auth/LoginView.vue'
 import RegisterView from '@/views/auth/RegisterView.vue'
 import DriverRegistrationView from '@/views/auth/DriverRegistrationView.vue'
+import SampleForm from '@/views/auth/SampleView.vue'
+import HomeView from '@/views/auth/HomeView.vue'
+import MessageView from '@/views/auth/MessageView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,6 +24,21 @@ const router = createRouter({
       path: '/driver-register',
       name: 'register as driver',
       component: DriverRegistrationView,
+    },
+    {
+      path: '/sample',
+      name: 'Sample',
+      component: SampleForm,
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: HomeView,
+    },
+    {
+      path: '/message',
+      name: 'Message',
+      component: MessageView,
     },
   ],
 })
