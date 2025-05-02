aa1c11d775fbd80fcd7460a17de3d8a5e4cd43e1
Jeremy Daniel Estrada <jeremydanielestrada2001@gmail.com>
2025-05-02 08:22:46 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,25 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/',
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    headers: {
+      'Cache-Control': 'public, max-age=31536000',
     },
   },
 })
